9a1e2b1d3d5d7953b3039ac0b57ed1bb09b987ee
OmidMircoram <94608450+OmidMircoram@users.noreply.github.com>
2023-11-10 14:51:25 +0100

    Delete config file

--- src/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: "AIzaSyCFEHnMuW3JCFS6kFWGW1MDJ9F1Sr9jIcg",
-    authDomain: "bookaspa-info-48149.firebaseapp.com",
-    projectId: "bookaspa-info-48149",
-    storageBucket: "bookaspa-info-48149.appspot.com",
-    messagingSenderId: "999641841037",
-    appId: "1:999641841037:web:3b674af9df150d80ba11c4"
-}
